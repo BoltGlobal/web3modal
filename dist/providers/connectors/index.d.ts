dcb862578d8d021836eccd005379ce483b208379
Jin Hao <limjinhao@gmail.com>
2021-07-30 17:21:18 +0800

    build version

---
+++ dist/providers/connectors/index.d.ts
@@ -0,0 +1,15 @@
+import injected from "./injected";
+import boltx from "./boltx";
+import walletconnect from "./walletconnect";
+import portis from "./portis";
+import fortmatic from "./fortmatic";
+import torus from "./torus";
+import arkane from "./arkane";
+import authereum from "./authereum";
+import burnerconnect from "./burnerconnect";
+import mewconnect from "./mewconnect";
+import dcentwallet from "./dcentwallet";
+import bitski from "./bitski";
+import frame from "./frame";
+export { injected, walletconnect, boltx, portis, torus, fortmatic, arkane, authereum, burnerconnect, mewconnect, dcentwallet, bitski, frame };
+//# sourceMappingURL=index.d.ts.map
